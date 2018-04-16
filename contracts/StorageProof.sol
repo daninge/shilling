58860040afd2773381c3f6cc37329a1c5e15f77e
Daniel Inge <daninge98@gmail.com>
2018-04-16 17:04:49 -0400

    barebones storageproof contract

---
+++ contracts/StorageProof.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.17;
+
+contract StorageProof {
+    address public requestor;
+    address public storer;
+    uint public fileId;
+    uint public challenge;
+
+    bytes proof;
+
+    function Migrations(address storerIn, uint fileIdIn) public {
+        requestor = msg.sender;
+        storer = storerIn;
+        fileId = fileIdIn;
+    }
+
+    function getStorer() view public returns (address) {
+        return storer;
+    }
+
+    function getRequestor() view public returns (address) {
+        return requestor;
+    } 
+
+    function getChallenge() view public returns (uint) {
+        return challenge;
+    }
+
+    function getFileId() view public returns (uint) {
+        return fileId;
+    }
+
+    function submitProof(bytes proofIn) public {
+        if(msg.sender == storer){
+            proof = proofIn;
+        }
+    }
+}
