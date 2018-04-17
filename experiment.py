1fbcef313a4edef1538c8937857fa8775ae94d1e
Daniel Inge <daninge98@gmail.com>
2018-04-17 02:04:28 -0400

    added genesis contract

--- experiment.py
+++ experiment.py
@@ -3,9 +3,18 @@ w3 = Web3(HTTPProvider('http://127.0.0.1:7545'))
 #print(w3.eth.blockNumber)
 
 import setup as s
+import time
+# receipt = s.deploy_contract(w3, "StorageProof", w3.eth.accounts[1], args=[69, 69])
+# print(receipt)
+# my_contract = s.get_contract_instance(w3, receipt['contractAddress'], "StorageProof")
+# print(my_contract.submitProof(b'abc', transact={'from': w3.eth.accounts[1]}))
+# while 
+# print(my_contract.getProof())
 
-receipt = s.deploy_contract(w3, "StorageProof", w3.eth.accounts[1], args=[69, 69])
-print(receipt)
-my_contract = s.get_contract_instance(w3, receipt['contractAddress'], "StorageProof")
-print(my_contract.submitProof(b'abc', transact={'from': w3.eth.accounts[1]}))
-print(my_contract.getProof())
+while s.storage_proof_address == None:
+    continue
+
+time.sleep(10)
+
+s.get_contract_instance(w3, s.storage_proof_address, "StorageProof").setProof(b'maddy')
+
--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ path = "contracts/"
 contracts = {}
 contract_interfaces = {}
 
+storage_proof_address = None
+
 #import the source code
 for file_name in os.listdir(path):
     file = open(path+file_name, 'r')
@@ -31,7 +33,22 @@ def deploy_contract(w3, contract_name, sender, args=[1,1]):
     # Get tx receipt to get contract address
     return w3.eth.waitForTransactionReceipt(tx_hash)
 
+#Gets a concise contract instance for this contract
 def get_contract_instance(w3, address, contract_name):
     return w3.eth.contract(abi=contract_interfaces[contract_name]['abi'], address=address, ContractFactoryClass=ConciseContract)
 
-#print(contract_interfaces['StorageProof']['abi'])
+def block_received(block_number):
+    block = w3.eth.getBlock(block_number);
+    print(block)
+    for i in range(0, len(block.transactions)):
+        print("Processing Transaction "+ str(i)+ " from block "+str(block_number))
+        transaction = (w3.eth.getTransactionFromBlock(block.number, i))
+
+#Returns when a transaction occurs on this contract
+def monitor_contract(w3, address, beginning_block): #beginning block is the first block to search
+    printed = w3.eth.blockNumber #starting from now (i.e. don't print the whole chain every time)
+    while(True):
+        while printed < w3.eth.blockNumber:
+            printed+=1
+            print("RECEIVED BLOCK "+str(printed))
+            block_received(printed)
