2cae4e59a0fbb59862a4f6aa51e951da53d7a30e
Daniel Inge <daninge98@gmail.com>
2018-04-20 01:28:57 -0400

    fixed bug in check proof

--- pdp.py
+++ pdp.py
@@ -63,7 +63,9 @@ file = [1234, 5678, 9101, 1213]
 
 def key_gen():
     (pubkey, privkey) = rsa.newkeys(512)
+    #pk = (N, g)
     pk = (privkey.n, g(privkey.p, privkey.q))
+    #sk = (e, d, v)
     sk = (privkey.e, privkey.d, random.getrandbits(512))
     return (pk, sk)
 
@@ -113,12 +115,22 @@ def gen_proof(pk, f, chal, T):
     print("wfu")
     #Multiply challeneges
     big_t = 1
-    for i in range(0, c):
-        big_t *= T[challenge_blocks[i]][0] ** coefficients[i]
 
     temp = 0
     for i in range(0, c):
         temp += coefficients[i] * get_message(challenge_blocks[i])
+
+    for i in range(0, c):
+        wi = str(sk[2] + (challenge_blocks[i] << 512)).encode('utf-8')
+        #print(wi)
+        #print("///////////")
+        h = hashlib.sha256()
+        h.update(wi)
+        hash_output = int(h.hexdigest(), 16) % pk[0]
+        big_t *= (hash_output ** coefficients[i])
+    big_t *= temp
+    big_t = pow(big_t, sk[1], pk[0])
+        #big_t *= T[challenge_blocks[i]][0] ** coefficients[i]
     
     print("john")
     h = hashlib.sha256()
@@ -127,11 +139,32 @@ def gen_proof(pk, f, chal, T):
     print("rho")
     return (big_t, rho)
 
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    g, y, x = egcd(b%a,a)
+    return (g, x - (b//a) * y, y)
+
+def mod_inv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('No modular inverse')
+    return x%m
+
+#a/b mod m
+def mod_divide(a, b, m):
+    a = a % m
+    inv = mod_inv(b, m)
+    if inv == -1:
+        print("oh no")
+    else:
+        return (inv * a) % m
+    
 def check_proof(pk, sk, chal, V):
     c, k1, k2, gs = chal
     print("before")
     # curvy_t = T * e
-    curvy_t = Decimal(pow(V[0], sk[0]))
+    curvy_t = pow(V[0], sk[0], pk[0])
     print("after")
     #generate challenge blocks
     challenge_blocks = get_challenge_blocks(k1, c, len(file))
@@ -147,9 +180,15 @@ def check_proof(pk, sk, chal, V):
         h.update(wi)
         hash_output = int(h.hexdigest(), 16) % pk[0]
         print("hmml")
-        curvy_t = (curvy_t / pow(hash_output, coefficients[i])) % pk[0]
-    print(curvy_t)
-    if pow(curvy_t, chal[3]) % pk[0] == V[1]:
+        curvy_t = mod_divide(curvy_t, pow(hash_output, coefficients[i]), pk[0])
+        #curvy_t = (curvy_t / pow(hash_output, coefficients[i])) % pk[0]
+    #print("curvy t = "+str(curvy_t))
+
+    h = hashlib.sha256()
+    h.update(str(pow(curvy_t, chal[3], pk[0])).encode('utf-8'))
+    hash_output = int(h.hexdigest(), 16) % pk[0]
+    print(hash_output)
+    if hash_output == V[1]:
         return True
     else:
         return False
@@ -169,7 +208,9 @@ for i in range(0, len(file)):
 chal = (2, 2, 4, pk[1] ** 4)
 #print(tags)
 print("here")
+#print("e "+)
 proof = gen_proof(pk, len(file), chal, tags)
+print("proof")
 print(proof)
 chal = (2, 2, 4, 4)
 
