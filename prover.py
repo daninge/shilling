b4d52949a95759023a136be896ff13102d02319b
wfu <wfu@college.harvard.edu>
2018-05-03 13:58:28 -0400

    [refactor] Made it so code doesn't run when importing

--- prover.py
+++ prover.py
@@ -53,29 +53,34 @@ def verify_proof_chain(merkle_root, proofs, chains, initial_challenge):
 proofs, chains = build_proof_chain("somefile.txt", 5, 5)
 print(verify_proof_chain(4,proofs, chains, 5))
 exit()
-#####################################################################
-#Logic starts here
 
-prover_id = w3.eth.accounts[5]
 
-#Get the genesis contract
-genesis_contract = s.get_contract_instance(w3, s.genesis_address, "GenesisContract")
 
-print("Waiting for a contract:")
 
-contract_address = genesis_contract.getOutsourcingContract()
-while contract_address == None:
-    #print(genesis_contract.getOutsourcingContract())
-    time.sleep(1)
+if __name__ == '__main__':
+    #####################################################################
+    #Logic starts here
+
+    prover_id = w3.eth.accounts[5]
+
+    #Get the genesis contract
+    genesis_contract = s.get_contract_instance(w3, s.genesis_address, "GenesisContract")
+
+    print("Waiting for a contract:")
+
     contract_address = genesis_contract.getOutsourcingContract()
+    while contract_address == None:
+        #print(genesis_contract.getOutsourcingContract())
+        time.sleep(1)
+        contract_address = genesis_contract.getOutsourcingContract()
 
-print("Accepting outsourcing contract at address "+str(contract_address))
+    print("Accepting outsourcing contract at address "+str(contract_address))
 
-#Get an instance of the contract at the accepted address
-current_contract = s.get_contract_instance(w3, contract_address, "OutsourcingContract")
-current_contract.setProvider(prover_id, transact={'from': prover_id})
+    #Get an instance of the contract at the accepted address
+    current_contract = s.get_contract_instance(w3, contract_address, "OutsourcingContract")
+    current_contract.setProvider(prover_id, transact={'from': prover_id})
 
-print(build_proof_chain("somefile.txt", current_contract.getInitialChallenge(), 3))
+    print(build_proof_chain("somefile.txt", current_contract.getInitialChallenge(), 3))
 
 
 
